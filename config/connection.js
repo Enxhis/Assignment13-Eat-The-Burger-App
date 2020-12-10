6b66868ed9ae12264d9fefc941a3eff2c75a8e3a
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-10 11:53:14 -0500

    setup connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up SQL connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "PASWORD GOES HERE",
+    database: "burgers_db"
+});
+
+// Make connection
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting: " + err.stack);
+        return;
+    }
+    console.log("Connected as id: " + connection.threadId);
+});
+
+// Export connection for ORM to use
+module.exports = connection;
