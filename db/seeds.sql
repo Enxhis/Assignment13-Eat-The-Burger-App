c8a8f8921250b99b036febc227fa5d6555d481a1
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-10 11:41:44 -0500

    populated the db table

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+-- Data Entries for the burgers table
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("turkey burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("beef burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("whopper", FALSE);
