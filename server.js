4d0cf79de4549fbd3d34c56d2cd9ada311ca6ab0
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-10 10:43:41 -0500

    initial commit & directory structure

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+var express = require("express");
+
+// PORT
+var PORT = process.env.PORT || 3030;
+
+// App
+var app = express();
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app/use(express.json());
+
+// set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+// TODOO...
+var routes = require("./controllers");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Apllication listening at localhost:" + PORT);
+});
