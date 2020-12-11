6c6351847b9789bbf9abeab58b166de89d0cd5fa
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-11 11:45:15 -0500

    POST & PUT routes added

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,11 +7,36 @@ var burger = require("../models/burgers.js");
 
 // Create all our routes and set up logic within routes
 // GET route
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
         res.render("index", hbsObject);
     });
 });
+
+// POST route
+router.post("/burgers", function (req, res) {
+    burger.insertOne(["burger_name"], [req.body.burger_name], function(result){
+        res.redirect("/");
+    });
+});
+
+// PUT route
+router.put("/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition,
+    function(result){
+        if(result.changedRows === 0){
+            return res.status(404).end();
+        }
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use
+module.exports = router;
