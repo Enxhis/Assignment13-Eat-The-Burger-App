796007a62a0a128dfaee4e8a5ab171089c8093d3
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-12-16 13:38:44 -0500

    changes to connection

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "myloveReni2015",
+        password: "PASSWORD HERE",
         database: "burgers_db"
     });
 }
